cf2fd8cbf5e59a9ac9879293d040e920cb2840af
giorgiodaniele <giorgiodaniele15@gmail.com>
2026-05-07 16:58:37 +0200

    Fixed terraform files

--- code/terraforms/vm/main.tf
+++ code/terraforms/vm/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-app-dev-weu-01"
+  name     = "rg-vmapp-dev-weu-01"
   location = "norwayeast"
 }
 
--- code/terraforms/wapp/main.tf
+++ code/terraforms/wapp/main.tf
@@ -15,6 +15,31 @@ resource "azurerm_service_plan" "plan" {
   sku_name            = "B1"
 }
 
+#
+# Create a new workspace where application insights is going
+# to write
+#
+
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "law-wapp-dev-weu-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_application_insights" "ai" {
+  name                = "appi-wapp-dev-weu-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  workspace_id        = azurerm_log_analytics_workspace.law.id
+  application_type    = "web"
+}
+
+#
+# Create webapp
+#
+
 resource "azurerm_linux_web_app" "webapp" {
   name                = "app-wapp-dev-weu-01"
   location            = azurerm_resource_group.rg.location
@@ -22,4 +47,10 @@ resource "azurerm_linux_web_app" "webapp" {
   service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {}
+
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY"             = azurerm_application_insights.ai.instrumentation_key
+    "APPLICATIONINSIGHTS_CONNECTION_STRING"      = azurerm_application_insights.ai.connection_string
+    "ApplicationInsightsAgent_EXTENSION_VERSION" = "~3"
+  }
 }
